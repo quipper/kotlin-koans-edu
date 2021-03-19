5de5a8f7aa8bc225710d3a5f26dd682f3534b469
Wisnu Priyambodo <wisnu.priyambodo@quipper.com>
2021-03-19 15:54:50 +0700

    update introduction

--- Collections/Introduction/test/tests.kt
+++ Collections/Introduction/test/tests.kt
@@ -1,8 +1,23 @@
 import org.junit.Assert
 import org.junit.Test
 import koans.util.errorMessage
+import org.junit.Assert.assertEquals
 
 class TestIntroduction {
+    @Test(timeout = 1000)
+    fun testEmptyCustomers() {
+        Assert.assertTrue(emptyCustomerList().isEmpty())
+    }
+
+    @Test(timeout = 1000)
+    fun testListOfCustomers() {
+        val customer1 = shop.customers[0]
+        val customer2 = shop.customers[1]
+        val customerList = createCustomerList(customer1, customer2)
+        assertEquals(customer1, customerList[0])
+        assertEquals(customer2, customerList[1])
+    }
+
     @Test(timeout = 1000)
     fun testSetOfCustomers() {
         Assert.assertTrue(errorMessage("getSetOfCustomers"),
